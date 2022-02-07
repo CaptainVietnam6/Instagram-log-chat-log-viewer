ab2cec1a7f4edc9e1da70c12015f739356772c99
CaptainVietnam6 <kiet.riley2005@gmail.com>
2022-02-06 21:03:40 -1000

    added copyright message and license

--- statistics_finder.py
+++ statistics_finder.py
@@ -1,3 +1,17 @@
+'''
+Copyright (©) 2022 Kiet Pham <kiet.riley2005@gmail.com>
+This software/program has a copyright license, more information is in the 'LICENSE' file
+IF YOU WANT TO USE/COPY/MODIFY/REPRODUCE/RE-DISTRIBUTE THIS PROGRAM, YOU MUST INCLUDE A COPY OF THE LICENSE
+
+Author Name: Kiet Pham
+Author Contact: kiet.riley2005@gmail.com
+Discord: CaptainVietnam6#0001
+Discord Server: https://discord.gg/3z76p8H5yj
+GitHub: https://github.com/CaptainVietnam6
+Instagram: @itzkiettttt_fpv
+Program Status: ACTIVE/FINALISED/ABANDONED/OUTDATED
+'''
+
 import json
 from timeit import default_timer as timer
 from unicodedata import name
